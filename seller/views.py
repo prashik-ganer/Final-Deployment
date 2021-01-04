76511cdb3aa0d2aaaff61fbd98362cd2f2eb10b5
Prashik <prashik.ganer@gmail.com>
2021-01-04 19:56:50 +0530

    removed cv2 from seller/views

--- seller/views.py
+++ seller/views.py
@@ -13,10 +13,7 @@ import json
 import pyqrcode
 from .filters import OrdersFilter
 
-import cv2
-import numpy as np
-import urllib.request
-from pyzbar.pyzbar import decode 
+
 
 
 # Create your views here.
@@ -212,67 +209,6 @@ def qrcode(request):
 
     # context = {'image_name':image_name, 'image_qrcode':image_qrcode}
     return HttpResponse("qrcode!")
-
-
-# def scanqr(request):
-    # # img = cv2.imread('image.PNG')
-
-    # url = 'http://192.168.43.8/cam-lo.jpg'
-    # cv2.namedWindow("window", cv2.WINDOW_AUTOSIZE)
-
-    # with open('myfile.txt') as f:
-    #     myDataList = f.read().splitlines()
-    # print(myDataList)
-
-    # while True:
-    #     imgResponse = urllib.request.urlopen(url)
-    #     imgnp = np.array(bytearray(imgResponse.read()), dtype=np.uint8)
-    #     # print("imgnp  :  ",imgnp)
-    #     img = cv2.imdecode(imgnp, -1)
-    #     # print("img : ", img)
-    #     for qrcode in decode(img):
-    #         # print(qrcode.data)
-    #         myData = qrcode.data.decode('utf-8')
-    #         print("mydata : ", myData)
-    #         file1 = open('sample.txt','a')
-    #         file1.write(myData)
-    #         pts = np.array([qrcode.polygon],np.int32)
-    #         pts = pts.reshape((-1,1,2))
-    #         cv2.polylines(img,[pts],True,(204,102,0),5)
-
-    #         pts2 = qrcode.rect
-    #         cv2.putText(img,myData,(pts2[0],pts2[1]), cv2.FONT_HERSHEY_SIMPLEX,0.9,(204,102,0),2)
-    #     # gray = cv2.cvtColor(img, cv2.COLOR_BayerBG2GRAY)
-
-    #     cv2.imshow("window", img)
-    #     key = cv2.waitKey(5)
-    #     if key == ord('q'):
-    #         break
-
-    # cv2.destroyAllWindows
-
-
-    # return HttpResponse("ScanQr")
-
-def scanqr(request):
-    url= 'http://192.168.43.8/cam-lo.jpg'
-    cv2.namedWindow("Window", cv2.WINDOW_AUTOSIZE)
-
-    while True:
-        image = urllib.request.urlopen(url)
-        imgnp = np.array(bytearray(image.read()),dtype=np.uint8)
-        img = cv2.imdecode(imgnp, -1)
-        for qrcode in decode(img):
-            myData = qrcode.data.decode('utf-8')
-            print("mydata : ", myData)
-            pts = np.array([qrcode.polygon],np.int32)
-            pts = pts.reshape((-1,1,2))
-            cv2.polylines(img,[pts],True,(255,0,255),5)
-            pts2 = qrcode.rect
-            cv2.putText(img,myData,(pts2[0],pts2[1]), cv2.FONT_HERSHEY_SIMPLEX,0.9,(255,0,255),2)
-        cv2.imshow('Result : ', img)
-        cv2.waitKey(1)
-    cap.release()
 
 
 
